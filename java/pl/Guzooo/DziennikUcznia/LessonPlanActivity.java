372e8aabc4ce41c6246af9040856fa832ee04039
Guzooo <guzooo-guzooo@o2.pl>
2018-10-20 10:33:36 +0200

    0.2.7
    Ustawiono rozmiary z pliku dimensions w Activity planu lekcji
    Zmieniono układ Activity planu lekcji by dolny przycisk był nad wyświetlaną treśnią
    Uporządkowano metody w Activity planu lekcji

--- java/pl/Guzooo/DziennikUcznia/LessonPlanActivity.java
+++ java/pl/Guzooo/DziennikUcznia/LessonPlanActivity.java
@@ -3,18 +3,18 @@ package pl.Guzooo.DziennikUcznia;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.view.ViewTreeObserver;
 import android.widget.Toast;
 
 public class LessonPlanActivity extends Activity {
+
+    private RecyclerView recyclerView;
 
     private Cursor cursor;
     private SQLiteDatabase db;
@@ -23,50 +23,82 @@ public class LessonPlanActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson_plan);
+
+        recyclerView = findViewById(R.id.plan_recycler);
+
+        try {
+            db = StaticMethod.getReadableDatabase(this);
+            refreshCursor();
+            setAdapter();
+        }catch (SQLiteException e){
+            Toast.makeText(this, R.string.error_database, Toast.LENGTH_SHORT).show();
+        }
+        goFirstChangeView();
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-
-        RecyclerView recyclerView = findViewById(R.id.plan_recycler);
+    protected void onRestart() {
+        super.onRestart();
 
         try {
-            SQLiteOpenHelper openHelper = new HelperDatabase(this);
-            db = openHelper.getReadableDatabase();
-            cursor = db.query("LESSON_PLAN",
-                    SubjectPlan.subjectPlanOnCursor,
-                    null, null, null, null,
-                    "DAY, TIME_START");
-
-            LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-            recyclerView.setLayoutManager(layoutManager);
-            AdapterPlanCardView adapter = new AdapterPlanCardView(cursor, findViewById(R.id.plan_plan_null));
-            recyclerView.setAdapter(adapter);
-
-            adapter.setListener(new AdapterPlanCardView.Listener() {
-                @Override
-                public void onClick(int id) {
-                    Intent intent = new Intent(getApplicationContext(), LessonPlanEditActivity.class);
-                    intent.putExtra(LessonPlanEditActivity.EXTRA_ID, id);
-                    startActivity(intent);
-                }
-            });
+            refreshCursor();
+            setAdapter();
         }catch (SQLiteException e){
             Toast.makeText(this, R.string.error_database, Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onDestroy() {
+        super.onDestroy();
 
         cursor.close();
         db.close();
     }
 
     public void ClickAdd(View v){
-        Intent intent = new Intent(this, LessonPlanEditActivity.class);
+        goToLessonPlanEditActivity(0);
+    }
+
+    private void goFirstChangeView(){
+        ViewTreeObserver viewTreeObserver = recyclerView.getViewTreeObserver();
+
+        if (viewTreeObserver.isAlive()){
+            viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @Override
+                public void onGlobalLayout() {
+                    recyclerView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    View bottomButtons = findViewById(R.id.plan_bottom_button);
+                    recyclerView.setPadding(recyclerView.getPaddingLeft(), recyclerView.getPaddingTop(), recyclerView.getPaddingRight(), bottomButtons.getHeight());
+                }
+            });
+        }
+    }
+
+    private void refreshCursor(){
+        cursor = db.query("LESSON_PLAN",
+                SubjectPlan.subjectPlanOnCursor,
+                null, null, null, null,
+                "DAY, TIME_START");
+    }
+
+    private void setAdapter(){
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        AdapterPlanCardView adapter = new AdapterPlanCardView(cursor, findViewById(R.id.plan_plan_null));
+        recyclerView.setAdapter(adapter);
+
+        adapter.setListener(new AdapterPlanCardView.Listener() {
+            @Override
+            public void onClick(int id) {
+                goToLessonPlanEditActivity(id);
+            }
+        });
+    }
+
+    private void goToLessonPlanEditActivity(int id){
+        Intent intent = new Intent(getApplicationContext(), LessonPlanEditActivity.class);
+        intent.putExtra(LessonPlanEditActivity.EXTRA_ID, id);
         startActivity(intent);
     }
 }
