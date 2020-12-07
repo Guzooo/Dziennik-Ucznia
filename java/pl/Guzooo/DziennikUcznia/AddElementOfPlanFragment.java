85da3a6713becf371b913e70f1b6d35ff79898e2
Guzooo <guzooo-guzooo@o2.pl>
2020-12-07 08:46:28 +0100

    0.7.0.8
    Poprawiono AlertDialog przy dodawaniu przedmitu lub elementu planu lekcji

--- java/pl/Guzooo/DziennikUcznia/AddElementOfPlanFragment.java
+++ java/pl/Guzooo/DziennikUcznia/AddElementOfPlanFragment.java
@@ -1,6 +1,5 @@
 package pl.Guzooo.DziennikUcznia;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.DialogInterface;
@@ -16,6 +15,7 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentManager;
 
